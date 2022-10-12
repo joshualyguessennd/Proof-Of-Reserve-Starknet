bc3a6497834592285cab617ef8208eff6b4459fb
joshualyguessennd <joshualyguessennd@icloud.com>
2022-10-12 14:36:45 +0000

    fix review 1

--- test/messaging.test.ts
+++ test/messaging.test.ts
@@ -1,28 +1,69 @@
-import { solidity } from "ethereum-waffle";
-import chai, { assert, expect } from "chai";
-import { simpleDeploy } from "@makerdao/hardhat-utils";
-import hre, { ethers, network, starknet } from "hardhat";
+
+import { expect } from "chai";
+import hre, { ethers, starknet } from "hardhat";
 import { StarknetContract, HardhatUserConfig } from "hardhat/types";
 import config from "../hardhat.config";
+import { Contract, ContractFactory } from "ethers";
+import { Account } from "hardhat/types";
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+import { getSelectorFromName } from "starknet/dist/utils/hash";
 
 describe("test contracts interaction", function () {
+    let l1Contract: Contract;
+    let l2Contract: StarknetContract;
+    let l1ContractFactory: ContractFactory;
+    let l2user: Account;
+    let selector: string;
+    let mockStarknetMessagingAddress: string;
+    let admin: SignerWithAddress;
+    let l1_user: SignerWithAddress;
+
+
+    before(async () => {
+        [admin, l1_user] = await hre.ethers.getSigners();
+        // const starkNetFake = await smock.fake(interface);
+        l2user = await starknet.deployAccount("OpenZeppelin");
+
+        const networkUrl =
+            (config as HardhatUserConfig).networks?.l1_testnet?.url ||
+            "http://localhost:8545";
+
+        mockStarknetMessagingAddress = (
+            await starknet.devnet.loadL1MessagingContract(networkUrl)
+        ).address;
+
+        const l2ContractFactory = await starknet.getContractFactory(
+            "contracts/starknet/ProofReserve"
+        );
+
+        l2Contract = await l2ContractFactory.deploy({ admin: l2user.starknetContract.address, publisher: BigInt(761466874539515783303110363281120649054760260892n) });
+
+        l1ContractFactory = await ethers.getContractFactory("L1_CONTRACT", admin);
+        selector = getSelectorFromName("post_data")
+        l1Contract = await l1ContractFactory.deploy(
+            mockStarknetMessagingAddress,
+            BigInt(l2Contract.address),
+            BigInt(selector)
+        )
+        await l1Contract.deployed();
+    })
 
     it("check deployment data", async () => {
-        const { l1Contract, l2Contract, l2user, mockStarknetMessagingAddress, admin, l1_user } = await setupTest();
         const _selector = await l1Contract.SELECTOR();
-        expect(_selector).to.be.eq(BigInt(1447471469451581726846414018284835017311951023166934288557129659906920235665))
+
+        expect(_selector).to.be.eq(BigInt(selector))
         expect(await l1Contract.l2Contract()).to.be.eq(BigInt(l2Contract.address));
         expect(await l1Contract.countPublishers()).to.be.eq(0);
         // wrong access
         expect(l1Contract.connect(l1_user).addNewPublisher(admin.address)).to.rejected;
 
-        const tx = await l1Contract.connect(admin).addNewPublisher(admin.address);
+        await l1Contract.connect(admin).addNewPublisher(admin.address);
         expect(await l1Contract.countPublishers()).to.be.eq(1);
 
     })
 
     it("check publish data", async () => {
-        const { l1Contract, l2Contract, l2user, mockStarknetMessagingAddress, admin, l1_user } = await setupTest();
+
         await l1Contract.connect(admin).addNewPublisher(admin.address);
         // expect it to fail  with is Not Signer
         expect(l1Contract.connect(l1_user).publishData(
@@ -64,43 +105,43 @@ describe("test contracts interaction", function () {
 })
 
 
-async function setupTest() {
-    const [admin, l1_user] = await hre.ethers.getSigners();
-    // const starkNetFake = await smock.fake(interface);
-    const l2user = await starknet.deployAccount("OpenZeppelin");
+// async function setupTest() {
+//     const [admin, l1_user] = await hre.ethers.getSigners();
+//     // const starkNetFake = await smock.fake(interface);
+//     const l2user = await starknet.deployAccount("OpenZeppelin");
 
-    const networkUrl =
-        (config as HardhatUserConfig).networks?.l1_testnet?.url ||
-        "http://localhost:8545";
+//     const networkUrl =
+//         (config as HardhatUserConfig).networks?.l1_testnet?.url ||
+//         "http://localhost:8545";
 
-    const mockStarknetMessagingAddress = (
-        await starknet.devnet.loadL1MessagingContract(networkUrl)
-    ).address;
-    let l2Contract: StarknetContract;
+//     const mockStarknetMessagingAddress = (
+//         await starknet.devnet.loadL1MessagingContract(networkUrl)
+//     ).address;
+//     let l2Contract: StarknetContract;
 
-    const l2ContractFactory = await starknet.getContractFactory(
-        "contracts/starknet/ProofReserve"
-    );
+//     const l2ContractFactory = await starknet.getContractFactory(
+//         "contracts/starknet/ProofReserve"
+//     );
 
-    l2Contract = await l2ContractFactory.deploy({ admin: l2user.starknetContract.address, publisher: BigInt(761466874539515783303110363281120649054760260892n) });
+//     l2Contract = await l2ContractFactory.deploy({ admin: l2user.starknetContract.address, publisher: BigInt(761466874539515783303110363281120649054760260892n) });
 
-    // const l2Contract = 31415;
-    const l1Contract = await simpleDeploy("L1_CONTRACT", [
-        mockStarknetMessagingAddress,
-        BigInt(l2Contract.address),
-        BigInt(1447471469451581726846414018284835017311951023166934288557129659906920235665)
-    ]);
+//     // const l2Contract = 31415;
+//     const l1Contract = await simpleDeploy("L1_CONTRACT", [
+//         mockStarknetMessagingAddress,
+//         BigInt(l2Contract.address),
+//         BigInt(1447471469451581726846414018284835017311951023166934288557129659906920235665)
+//     ]);
 
-    console.log("messaging address is", mockStarknetMessagingAddress);
+//     console.log("messaging address is", mockStarknetMessagingAddress);
 
-    // await l2user.invoke(l2Contract, "set_l1", { l1_address: l1Contract.address });
-    return {
-        l1Contract: l1Contract as any,
-        l2Contract: l2Contract as any,
-        l2user: l2user as any,
-        mockStarknetMessagingAddress: mockStarknetMessagingAddress as any,
-        // starkNetFake: starkNetFake as any,
-        admin: admin as any,
-        l1_user: l1_user as any
-    };
-}
+//     // await l2user.invoke(l2Contract, "set_l1", { l1_address: l1Contract.address });
+//     return {
+//         l1Contract: l1Contract as any,
+//         l2Contract: l2Contract as any,
+//         l2user: l2user as any,
+//         mockStarknetMessagingAddress: mockStarknetMessagingAddress as any,
+//         // starkNetFake: starkNetFake as any,
+//         admin: admin as any,
+//         l1_user: l1_user as any
+//     };
+// }
